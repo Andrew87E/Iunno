4b55ddad3815fb5b0b7043cb1eae9cb5492e109c
andrew87e <andrew87e@gmail.com>
2022-09-28 11:18:16 -0500

    Initial Commit

---
+++ Iunno/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Iunno
+//
+//  Created by andrew on 9/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Iunno/IunnoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IunnoApp.swift
+//  Iunno
+//
+//  Created by andrew on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct IunnoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
